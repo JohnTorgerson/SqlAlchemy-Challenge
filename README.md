55791e535dc7a19eb358bc9f5618413b5e1698fe
John <107654605+JohnTorgerson@users.noreply.github.com>
2022-08-29 18:18:38 -0500

    Completed Flask app, cleaned up ReadME

--- README.md
+++ README.md
@@ -5,25 +5,17 @@
 ##### Authors:
 * John Torgerson (JohnTorgerson)
 ---
-
-##### Instructions:
-Create file `config.py` to pass your user name, password, and port number into variables
-This code will not run unless you first configure the following three variables:
-* user = "postgres"
-* password = "<YOUR PostgreSQL Password HERE>"
-* port = "<Your PostgreSQL Port Number HERE>"
----
-    
+   
 ##### Tools and Supplies:
 * < Database and table description here >
 
-* Python, SQLAlchemy, Pandas, Matplotlib; SQLAlchemy ORM session queriies were made from SQLite data
+* Python, SQLAlchemy, Pandas, Matplotlib and Flask; SQLAlchemy ORM session queriies were made from SQLite data and Flask sessions and jsonify
 ---
 
 ### Guide to Repo Contents:
 
-* `climate_starter.ipynb` is < describe contents >
-* `abc.png` is < describe image >
+* `climate_notebook.ipynb` is the jupyter notebook used with Matplotlib, Pandas, SQLAlchemy to explore the tables and execute the analysis
+* `app.py` contains the SQLite code for the Flask Endpoint app with SQLAlchemy and JSONify
 * In folder, `Resources` are the following 6 tables:
     1. `hawaii.sqlite` is a database of precipitation measurements at 9 stations
     2. `hawaii_measurements.csv` is a table of precip measurements over time from all stations
@@ -32,15 +24,15 @@ This code will not run unless you first configure the following three variables:
     5. `tobs_year.png` is a histogram showing time of observation changes over the most recent year
 ---
 
-### Observations:
-* < List Observations Here >
-* The relationship is inverse.
-* Engineer, Asst Engineer, Senior Engineer have similar AVG Salaries, with Senior Engineers making the least of the group. Meanwhile Senior Staff as well as general Staff, both AVG nearly 12,000 more. Also, from our previous examples, there are an awful lot of women named Hercules B____, which seems improbable.
-* Either something is wrong, or this company data is fictional.
-
+### Flask Endpoints:
+* /
+* /api/v1.0/precipitation
+* /api/v1.0/stations
+* /api/v1.0/tobs
+* /api/v1.0/<start_date>
+* /api/v1.0/<start_date>/<end_date>
 ---
 
 ### Credits and Special Thanks
 
 * Thanks to Sanoo Singh for helping me clean up my plot labels and legend
-* Thanks to 
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, desc
 
 from flask import Flask, jsonify
 
@@ -20,9 +20,9 @@ Base.prepare(engine, reflect=True)
 mea = Base.classes.measurement
 sta = Base.classes.station
 
-
-
-
+MI = func.min(mea.tobs)
+AV = func.avg(mea.tobs)
+MA = func.max(mea.tobs)
 
 #################################################
 # 2. Flask Setup
@@ -35,7 +35,15 @@ app = Flask(__name__)
 #################################################
 
 @app.route("/")
+
 def welcome():
+    session = Session(engine)
+
+    # desc tool currently not supported
+    first_date = session.query(mea.date).order_by(mea.date).first()[0]
+    #last_date = session.query(mea.date).order_by(mea.date.desc).first()[0]
+    last_date = '2017-08-23'
+
     return (
         f"Welcome to the Hawaii weather station program!</br></br><br/>"
         f"Available Routes:<br/>"
@@ -44,9 +52,9 @@ def welcome():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start> ( Enter a start date YYYY-MM-DD )<br/>"
         f"/api/v1.0/<start> ( Enter a start date YYYY-MM-DD ) /<end> ( and an additional end date YYYY-MM-DD)<br/><br/>"
-        f"For best results use a start date between 2010-01-01 & 2017-08-23"
+        f"For best results use a start date between {first_date} & {last_date} and enter them in chronological order"
     )
-
+    
 #################################################
 # 5. Setup Additional Endpt Routes
 #################################################
@@ -95,20 +103,78 @@ def tempobs():
 
     return jsonify(tbs)
 
-# Return another app route with user input varaibles
+# Return another app route with user start date input varaibles
 @app.route("/api/v1.0/<start_date>")
-def star(start_date):
-    """Fetch the min avg and max between available dates
+
+def query_by_startdate(start_date):
+    """Fetch the min avg and max temps between available dates
        as a path variable supplied by the user, or a 404 if not."""
 
-    canonicalized = start_date.replace(" ", "").lower()
-    for query in Base.classes.measurement:
-        search_term = session.query(mea.station, func.min(mea.tobs), func.avg(mea.tobs), func.max(mea.tobs)).filter(mea.date >= {start_date}).all()
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-        if search_term == canonicalized:
-            return jsonify(query)
+    # define barriers # desc currently not supported
+    first_date = session.query(mea.date).order_by(mea.date).first()[0]
+    #last_date = session.query(mea.date).order_by(mea.date.desc).first()
+    last_date = '2017-08-23'
 
-    return jsonify({"error": f"That is not a date recognized by this dataframe. Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD."}), 404
+    # #for query in mea:
+        #search = ['date']
+
+    if start_date > last_date:
+        return (f"Error: That is not a date recognized by this dataframe. 404<br/><br/>Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD.")
+
+    elif start_date < first_date:
+        return (f"Error: That is not a date recognized by this dataframe. 404<br/><br/>Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD.")
+        
+    else:
+        query = session.query(MI, AV, MA).filter(mea.date >= start_date).all()
+    
+        sd = list(np.ravel(query))
+        return jsonify(f"Min, Avg, Max temps from {start_date} to {last_date}.", sd)
+
+# Now add an end date
+@app.route("/api/v1.0/<start_date>/<end_date>")
+
+def query_by_end_date(start_date, end_date):
+    """Fetch the min avg and max temps between available dates
+       as a path variable supplied by the user, or a 404 if not."""
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # define barriers # desc currently not supported
+    first_date = session.query(mea.date).order_by(mea.date).first()[0]
+    #last_date = session.query(mea.date).order_by(mea.date.desc).first()
+    last_date = '2017-08-23'
+
+    # #for query in mea:
+        #search = ['date']
+
+    if start_date < first_date:
+        return (f"Error: That is not a date recognized by this dataframe. 404<br/><br/>Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD.")
+
+    elif start_date > last_date:
+        return (f"Error: That is not a date recognized by this dataframe. 404<br/><br/>Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD.")
+        
+    elif end_date < first_date:
+        return (f"Error: That is not a date recognized by this dataframe. 404<br/><br/>Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD.")
+
+    elif end_date < start_date:
+        return (f"Error: Those two dates are not chronological. 404<br/><br/>Please enter a startdate between 2010-01-01 and 2017-08-23, and in the format of YYYY-MM-DD.")
+
+    elif end_date < last_date:
+        query = session.query(MI, AV, MA).filter(mea.date >= start_date).filter(mea.date <= end_date).all()
+    
+        sd = list(np.ravel(query))
+        return jsonify(f"Min, Avg, Max temps from {start_date} to {end_date}.", sd)
+
+    else:
+        query = session.query(MI, AV, MA).filter(mea.date >= start_date).all()
+    
+        sd = list(np.ravel(query))
+        
+        return jsonify(f"Sorry. Laast Date in table is {last_date} - Min, Avg, Max temps from {start_date} to {last_date}.", sd)
 
 #################################################
 # 3. Run Main App debugger for main endpt
